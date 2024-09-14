d4df85ed46d4c3da8355ed61a1d65407a5382cc9
JSPENZ\Abell <abell.wandili@strathmore.edu>
2024-09-14 08:37:57 +0300

    Viewsets classes to manage CRUD operations for the 3 models based on their serializers and customize permissions based on admin and authenticated user roles

--- investment_management_api/investment_accounts/views.py
+++ investment_management_api/investment_accounts/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import InvestmentAccount, CustomerInvestmentAccount, Transaction
+from .serializers import InvestmentAccountSerializer, CustomerInvestmentAccountSerializer, TransactionSerializer
 
-# Create your views here.
+# Handles CRUD operations for InvestmentAccount based on its serializer class InvestmentAccountSerializer
+class InvestmentAccountViewSet(viewsets.ModelViewSet):
+    queryset = InvestmentAccount.objects.all()
+    serializer_class = InvestmentAccountSerializer
+
+# Handles CRUD operations for CustomerInvestmentAccount based on its serializer class CustomerInvestmentAccountSerializer
+class CustomerInvestmentAccountViewSet(viewsets.ModelViewSet):
+    queryset = CustomerInvestmentAccount.objects.all()
+    serializer_class = CustomerInvestmentAccountSerializer
+
+# Handles CRUD operations for Transaction based on its serializer class TransactionSerializer as well as handling additional permission requirements for authenticated users and admin roles
+class TransactionViewSet(viewsets.ModelViewSet):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+
+    def get_permissions(self):
+        if self.action == 'create':
+            return [permissions.IsAuthenticated()]
+        elif self.action in ['list', 'retrieve']:
+            return [permissions.IsAuthenticated()]
+        return [permissions.IsAdminUser()]
