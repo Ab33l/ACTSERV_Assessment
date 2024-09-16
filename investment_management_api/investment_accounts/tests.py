c0a9d542d5064eb8ef22cebd6b32e090aebae25c
JSPENZ\Abell <abell.wandili@strathmore.edu>
2024-09-16 20:25:48 +0300

    Unit Tests to validate API functionality

--- investment_management_api/investment_accounts/tests.py
+++ investment_management_api/investment_accounts/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from .models import Customer, InvestmentAccount, CustomerInvestmentAccount, Transaction
 
-# Create your tests here.
+class InvestmentAccountTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = Customer.objects.create_user(username='Abel', password='ActServ123')
+        self.client.login(username='Bob', password='ActServ321')
+
+    def test_create_investment_account(self):
+        response = self.client.post('/investment/investment-accounts/', {'name': 'KES Money Market Fund'})
+        self.assertEqual(response.status_code, 201)
+
+    def test_create_transaction(self):
+        account = InvestmentAccount.objects.create(name='KES_MMF_TopUp')
+        CustomerInvestmentAccount.objects.create(user=self.user, investment_account=account, permission='crud')
+        response = self.client.post('/investment/transactions/', {'investment_account': account.id, 'user': self.user.id, 'amount': 12340.10})
+        self.assertEqual(response.status_code, 201)
+
