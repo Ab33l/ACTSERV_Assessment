33d57076ed0dd44335f44194d76a8060dda98e48
JSPENZ\Abell <abell.wandili@strathmore.edu>
2024-09-16 20:26:37 +0300

    Unit Tests to validate the Investment Account Managemen API

--- investment_management_api/investment_accounts/tests.py
+++ investment_management_api/investment_accounts/tests.py
@@ -3,16 +3,16 @@ from rest_framework.test import APIClient
 from .models import Customer, InvestmentAccount, CustomerInvestmentAccount, Transaction
 
 class InvestmentAccountTests(TestCase):
-    def setUp(self):
+    def account_setup(self):
         self.client = APIClient()
         self.user = Customer.objects.create_user(username='Abel', password='ActServ123')
         self.client.login(username='Bob', password='ActServ321')
 
-    def test_create_investment_account(self):
+    def test_investment_account_creation(self):
         response = self.client.post('/investment/investment-accounts/', {'name': 'KES Money Market Fund'})
         self.assertEqual(response.status_code, 201)
 
-    def test_create_transaction(self):
+    def test_transaction_initiation(self):
         account = InvestmentAccount.objects.create(name='KES_MMF_TopUp')
         CustomerInvestmentAccount.objects.create(user=self.user, investment_account=account, permission='crud')
         response = self.client.post('/investment/transactions/', {'investment_account': account.id, 'user': self.user.id, 'amount': 12340.10})
